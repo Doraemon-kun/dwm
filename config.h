bed37a12730c49f355076635b847733804123379
Doraemon-kun <doraemon-kun@protonmail.com>
2021-08-23 16:57:33 +0700

    v1.2: New design

--- config.h
+++ config.h
@@ -8,8 +8,9 @@ static const unsigned int gapps	    = 3;	/* gaps size between windows */
 static const int smartgap	    = 1;	/* 1 means no outer gap when there is only one window */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar		    = 1;	/* 0 means bottom bar */
-static const char *fonts[]          = { "Noto Emoji:pixelsize=10:antialias=true:autohint=true", "Source Code Pro:size=10" };
-static const char dmenufont[]       = "Source Code Pro:size=10";
+static const int user_bh	    = 25;	/* 0 means that dwm will calculate bar height, >= 1 means dwm will set user_bh as bar height */
+static const char *fonts[]          = { "Noto Sans Mono:style=Bold:size=9", "Material Icons:style=Regular" };
+static const char dmenufont[]       = "Noto Sans Mono:style=Bold:size=9";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
@@ -26,12 +27,12 @@ static const char *colors[][3]      = {
 static const unsigned int alphas[][3]      = {
 	/*               fg      bg        border     */
 	[SchemeNorm] = { OPAQUE, baralpha, borderalpha },
-	[SchemeSel]  = { OPAQUE, baralpha, borderalpha },	
+	[SchemeSel]  = { OPAQUE, OPAQUE, borderalpha },	
 	[SchemeTitle] = { OPAQUE, baralpha, borderalpha },
 };
 
 /* tagging */
-static const char *tags[] = { "A", "B", "C", "D", "E", "F", "X", "Y", "Z" };
+static const char *tags[] = { " A ", " B ", " C ", " D ", " E ", " F ", " X ", " Y ", " Z " };
 
 static const Rule rules[] = {
 	/* xprop(1):
@@ -72,15 +73,14 @@ static const Layout layouts[] = {
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "kitty", NULL };
-static const char *volumeup[] = { "pactl", "set-sink-volume", "@DEFAULT_SINK@", "+10%", NULL };
-static const char *volumedown[] = { "pactl", "set-sink-volume", "@DEFAULT_SINK@", "-10%", NULL };
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
 	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
 	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
-	{ 0,             XF86XK_AudioRaiseVolume,      spawn,          {.v = volumeup } },
-	{ 0,             XF86XK_AudioLowerVolume,      spawn,          {.v = volumedown } },
+	{ 0,             XF86XK_AudioRaiseVolume,      spawn,          SHCMD("pactl set-sink-volume @DEFAULT_SINK@ +10%; kill -47 $(pidof dwmblocks)") },
+	{ 0,             XF86XK_AudioLowerVolume,      spawn,          SHCMD("pactl set-sink-volume @DEFAULT_SINK@ -10%; kill -47 $(pidof dwmblocks)") },
+	{ 0,             XF86XK_AudioMute,      spawn,          SHCMD("pactl set-sink-mute @DEFAULT_SINK@ toggle; kill -47 $(pidof dwmblocks)") },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
@@ -91,11 +91,11 @@ static Key keys[] = {
 	{ MODKEY,                       XK_Return, zoom,           {0} },
 	{ MODKEY,                       XK_Tab,    view,           {0} },
 	{ MODKEY|ShiftMask,             XK_c,      killclient,     {0} },
-	{ MODKEY,                       XK_t,      setlayout,      {.v = &layouts[0]} },
-	{ MODKEY,                       XK_f,      setlayout,      {.v = &layouts[1]} },
-	{ MODKEY,                       XK_m,      setlayout,      {.v = &layouts[2]} },
-	{ MODKEY,                       XK_u,      setlayout,      {.v = &layouts[3]} },
-	{ MODKEY,                       XK_o,      setlayout,      {.v = &layouts[4]} },
+	{ MODKEY|ShiftMask,                       XK_1,      setlayout,      {.v = &layouts[0]} },
+	{ MODKEY|ShiftMask,                       XK_2,      setlayout,      {.v = &layouts[1]} },
+	{ MODKEY|ShiftMask,                       XK_3,      setlayout,      {.v = &layouts[2]} },
+	{ MODKEY|ShiftMask,                       XK_4,      setlayout,      {.v = &layouts[3]} },
+	{ MODKEY|ShiftMask,                       XK_5,      setlayout,      {.v = &layouts[4]} },
 	{ MODKEY,                       XK_space,  setlayout,      {0} },
 	{ MODKEY|ShiftMask,             XK_space,  togglefloating, {0} },
 	{ MODKEY,                       XK_0,      view,           {.ui = ~0 } },
@@ -104,6 +104,7 @@ static Key keys[] = {
 	{ MODKEY,                       XK_period, focusmon,       {.i = +1 } },
 	{ MODKEY|ShiftMask,             XK_comma,  tagmon,         {.i = -1 } },
 	{ MODKEY|ShiftMask,             XK_period, tagmon,         {.i = +1 } },
+	{ MODKEY,			XK_e,	   spawn,	   SHCMD("firefox-developer-edition") },
 	TAGKEYS(                        XK_1,                      0)
 	TAGKEYS(                        XK_2,                      1)
 	TAGKEYS(                        XK_3,                      2)
